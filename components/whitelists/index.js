7faff074147f5276cc1d233ddead66359f02df8d
Tryjapa <thiago.yamaguchi@gmail.com>
2022-02-26 02:55:33 +0000

    add cssgrid to layouyt

--- components/whitelists/index.js
+++ components/whitelists/index.js
@@ -4,28 +4,19 @@ import styles from './index.module.css'
 
 
 const WhiteList = () => {
-    const [counter, setCounter] = React.useState(71140);
-    React.useEffect(() => {
-        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-      }, [counter]);
-      function secondsToHms(d) {
-        d = Number(d);
-        var h = Math.floor(d / 3600);
-        var m = Math.floor(d % 3600 / 60);
-        var s = Math.floor(d % 3600 % 60);
-    
-        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-        return hDisplay + mDisplay + sDisplay; 
-    }
-    return (
+        return (
         <div className={styles["container-white-list"]}>
-        
-        <div className={styles["box-white-list"]}>
-        <h2>Ending Soon... </h2>
-            <a href="https://giv.gg/r/XhtvdM-MXoBdm" target="_blank"><img className={styles["white-list-img"]}  alt="Whitelist NFT metaverse WorkTown Whitelist" src="https://assets.givelab.com/img/gi/XhtvdM.jpg?v=1638472621"></img>  </a>
-            <p>Ending in: {secondsToHms(counter)} </p>
+        <h2>Active IDO & Airdrops</h2>
+        <div className={styles["box-white-list"]}>        
+
+        <div className={styles["box-white-card"]}>
+            <div className={styles["box-white-card-img"]}><a href="https://giv.gg/r/XhtvdM-MXoBdm" target="_blank"><img className={styles["white-list-img"]}  alt="Whitelist NFT metaverse WorkTown Whitelist" src="https://assets.givelab.com/img/gi/XhtvdM.jpg?v=1638472621"></img>  </a></div>
+            <div className={styles["box-white-card-timer"]}><p>Ending in:  </p></div>
+            <div className={styles["box-white-card-details"]}> WorkTown</div>
+        </div>
+
+            
+            
         </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,33 @@ import styles from './index.module.css'
 import WhiteList from '../components/whitelists'
 
 function Home(){
-    return          <div  className={`${styles.waveWrapper} ${styles.waveAnimation}`}>
-        <title>NFT whitelist - Everything you can't miss about NFT, Metaverse and play to earn</title>
-        <h1 className={styles["text-floating-h1"]}> NFT - Whitelist</h1>
-        <h2 className={styles["text-floating-h2"]}> Never miss a whitelist again</h2>
+    return          <div  className={`${styles.container}`}>
+        <div className={`${styles.header}`}>
+        <title>NFT World - Whitelist - Everything you can't miss about NFT, Metaverse, Blockchain and play to earn</title>
+        <img className={`${styles.filters}`} src="/logo.png" alt="me" width="330" height="150" />
+        <a className={`${styles.adsHeader}`} href="https://rollercoin.com/?r=ky9fzosj">
+   <img src="//rollercoin.com/static/img/public_img/gen2/w728h90.gif"/> 
+ </a> 
+        </div>
+        <div className={`${styles.navBar}`}>
+          
+            
+            <div>Home</div>
+            <div>IDO Whitelist</div>
+            <div>Live Airdrops</div>
+            <div>About us</div>
+            <div>Partnerships</div>
+            <div>Contact us</div>
+            
+        </div>
+        <div className={`${styles.content}`}>
         <WhiteList></WhiteList>
-  <div  className={`${styles.waveWrapperInner} ${styles.bgTop}`}>
-    <div  className={`${styles.wave} ${styles.waveTop}`} ></div>
-  </div>
-  <div className={`${styles.wave} ${styles.waveTop}`} >
-    <div className={`${styles.wave} ${styles.waveTop}`}></div>
-  </div>
-  <div className={`${styles.waveWrapperInner} ${styles.bgBottom}`}>
-    <div className={`${styles.wave} ${styles.waveBottom}`} ></div>
-  </div>
+        </div>
+        <div className={`${styles.footer}`}></div>
+        
+       
+  
+  
 </div>
  
 }
